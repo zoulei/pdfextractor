c23f3e03e435a26d4900c5b405e2f34e0302cf7a
zoul15 <346954742@qq.com>
2023-08-08 00:29:58 +0800

    新增往pdf中添加目录和备考表的功能

--- setup.py
+++ setup.py
@@ -18,14 +18,15 @@ import py2exe
 # how to use :
 # under cmd : python setup.py py2exe
 # setup(console=["autoSplit.py","deleteABCD.py","mercolumn.py","gencover.py","checksplitresult.py","packfile.py","newgencover.py","autofillxlsfile.py","correctpdfdirection.py","mergepdf.py","blank_page_delete.py"])
-setup(console=["autoSplit_1.py", ], options={
+setup(console=["autoSplit_1.py", "add_catalog_beikaobiao.py"], options={
         "py2exe":{
              "dll_excludes":["libopenblas.TXA6YQSD3GCQQC22GEQ54J2UDCXDXHWN.gfortran-win_amd64.dll",
                              "libdet.KNZJ5W323CIP452TKKVK72OCSS32QOKE.gfortran-win_amd64.dll",
                              "libdop853.6TJTQZW3I3Q3QIDQHEOBEZKJ3NYRXI4B.gfortran-win_amd64.dll",
                              "libchkder.G7WSOGIYYQO3UWFVEZ3PPXCXR53ADVPA.gfortran-win_amd64.dll",
                              "libdcosqb.YMN7XEXYADIEZSKAGEVNR4E3MD7AXDG2.gfortran-win_amd64.dll",
-                             "libbispeu.7AH3PCQ2E2NGLC3AQD7FFAH73KGJTZCJ.gfortran-win_amd64.dll"],
+                             "libbispeu.7AH3PCQ2E2NGLC3AQD7FFAH73KGJTZCJ.gfortran-win_amd64.dll",
+                             "MSVCP90.dll"],
              "compressed":1,
              "optimize":2,
              # "bundle_files":1
